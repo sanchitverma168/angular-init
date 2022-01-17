299513823fedd55437e502cfcf1449dc2516ba29
Sanchit Verma <sanchit.verma.167@gmail.com>
2022-01-17 10:50:07 +0530

    Tailwind css integrated

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('postcss-import'),
+    require('tailwindcss'),
+    require('autoprefixer')
+  ],
+}
